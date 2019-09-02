75a7b196d025d9702c83d82cb1a9deb9a80208b4
Fándly Gergő <gergo@systemtest.tk>
2019-09-02 15:47:27 +0300

    Added school and user table

---
+++ backend/database/migrations/2019_09_02_000001_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('users', function (Blueprint $table) {
+			$table->bigIncrements('id');
+			$table->string('name');
+			$table->string('email')->unique();
+			$table->timestamp('email_verified_at')->nullable();
+			$table->string('password');
+			$table->bigInteger('school_id');
+			$table->tinyInteger('class'); // 0-no class, 1-3rd grade, 2-4th grade, 3-both
+			$table->tinyInteger('class_size');
+			$table->timestamps();
+
+			$table->foreign('school_id')->references('id')->on('schools');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists('users');
+	}
+}

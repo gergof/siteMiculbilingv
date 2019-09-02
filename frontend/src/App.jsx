27ca56d54bc09eb1d45430690bd96ecee0cd605f
Fándly Gergő <gergo@systemtest.tk>
2019-09-02 12:51:01 +0300

    Set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+	return <h1>Application, {process.env.API_ENDPOINT}</h1>;
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: process.env.NODE_ENV || 'development',
+	devtool: 'source-map',
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'index.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new Dotenv(),
+		new HtmlPlugin({
+			hash: true,
+			filename: 'index.html',
+			template: 'src/public/index.html'
+		})
+	],
+	devServer: {
+		historyApiFallback: true
+	}
+};

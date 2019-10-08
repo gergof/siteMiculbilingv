bef82b0eb5407cee8644dbf474cf8b69ee6857b9
Fándly Gergő <gergo@systemtest.tk>
2019-10-08 20:36:55 +0300

    Removed extra console log

--- frontend/src/scenes/Messages/components/NewMessage.jsx
+++ frontend/src/scenes/Messages/components/NewMessage.jsx
@@ -121,7 +121,6 @@ export const enhancer = compose(
 	),
 	withHandlers({
 		onSubmit: ({ sendMessage, history, usersStore }) => values => {
-			console.log(values);
 			sendMessage({
 				...values,
 				recipient: {
